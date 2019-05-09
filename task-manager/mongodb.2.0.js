34a219627f6acd96523be5dcd6c24c68a9dff531
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-05-09 20:00:38 +0200

    Change databas name to 'task-manager-api'

--- task-manager/mongodb.2.0.js
+++ task-manager/mongodb.2.0.js
@@ -7,7 +7,7 @@
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const databaseName = 'task-manager-api'
 
 // Async connection
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
