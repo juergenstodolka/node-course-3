16e89f935e17df6e9e7388865d9e022ab0d085fb
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-05-10 17:11:45 +0200

    add comments

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,27 +6,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 //Defining a model
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String
-//   },
-//   age: {
-//     type: Number
-//   }
-// })
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  }
+})
 
-// // Creating an instance
-// const me = new User({
-//   name: 'Jürgen',
-//   age: 'Mike'
-// })
+// Creating an instance
+const me = new User({
+  name: 'Juergen'
+})
 
-// // Save the instanct to the database
-// me.save().then(() => {
-//   console.log(me)
-// }).catch((error) => {
-//   console.log('Error!', error)
-// })
+// Save the instanct to the database
+me.save().then(() => {
+  console.log(me)
+}).catch((error) => {
+  console.log('Error!', error)
+})
 
 // -------------------------------------------------------------------
 // Goal: Create a model for tasks
@@ -37,22 +37,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // 4. Test yout work!
 // -------------------------------------------------------------------
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  }
-})
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// })
 
-const my_task = new Task({
-  description: 'Clean the kitchen',
-  completed: false
-})
+// const my_task = new Task({
+//   description: 'Clean the kitchen',
+//   completed: false
+// })
 
-my_task.save().then(() => {
-  console.log(my_task)
-}).catch((error) => {
-  console.log('Error!', error)
-})
+// my_task.save().then(() => {
+//   console.log(my_task)
+// }).catch((error) => {
+//   console.log('Error!', error)
+// })
